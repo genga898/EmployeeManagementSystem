27a8a633eae685de3fa27fa7520081db3dfe6b38
genga898 <genga898@gmail.com>
2023-12-31 17:43:49 +0300

    Update Program.cs
    Added services that will be umed  client application.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,14 +1,30 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using ClientLibrary.Helpers;
+using ClientLibrary.Services.Contracts;
+using ClientLibrary.Services.Implementations;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
+// builder.Services.AddScoped(sp => new HttpClient
+// {
+//     BaseAddress = new Uri("https://localhost:7275")
+// });
+builder.Services.AddTransient<CustomHttpHandler>();
+builder.Services.AddHttpClient("SystemApiClient", client =>
 {
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
+    client.BaseAddress = new Uri("https://localhost:7275");
+}).AddHttpMessageHandler<CustomHttpHandler>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<GetHttpClient>();
+builder.Services.AddScoped<LocalStorageService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<IUserAccountService, UserAccountService>();
 
 await builder.Build().RunAsync();
