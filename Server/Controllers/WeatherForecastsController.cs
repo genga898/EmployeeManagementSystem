05258352ad3f11c6d1fb5419457b78cdf320a6b1
genga898 <genga898@gmail.com>
2023-12-31 18:00:57 +0300

    Add WeatherForecastsController.cs

---
+++ Server/Controllers/WeatherForecastsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class WeatherForecastsController(ILogger<WeatherForecastsController> logger) : ControllerBase
+    {
+        private static readonly string[] Summaries = 
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<BaseLibrary.DTOs.WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new BaseLibrary.DTOs.WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
